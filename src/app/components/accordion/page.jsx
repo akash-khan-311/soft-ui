028298d307fea85b6fd67c519836a4f97e7350db
Akash Khan <akashcc48@gmail.com>
2024-12-13 21:51:30 +0600

    some file changes

--- src/app/components/accordion/page.jsx
+++ src/app/components/accordion/page.jsx
@@ -9,7 +9,7 @@ export const metadata = {
     "Soft UI React Components is a modern, reusable, and customizable library for React and Next.js. Build elegant and responsive web applications with ease using our comprehensive collection of UI components.",
   keywords:
     "Soft UI, React UI Components, Tailwind CSS, Customizable UI Library, Modern UI, Reusable Components, Next.js UI Library",
-  author: "Md Akash Ali",
+  author: "Md Akash Khan",
   openGraph: {
     type: "website",
     url: "https://softui-react.vercel.app",
